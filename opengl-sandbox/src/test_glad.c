c5bfcfb97b282a863db0671c3af13d20bc2d2543
seafoodfry <>
2024-06-08 15:15:04 -0400

    Render a window
    Signed-off-by: seafoodfry <>

--- opengl-sandbox/src/test_glad.c
+++ opengl-sandbox/src/test_glad.c
@@ -1,13 +1,47 @@
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main() {
+    // Instantiate the GLFW window.
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); needed on macos.
 
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout<< "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // Register the callback function for resizing.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Initialize GLAD to manage function pointer for openGL.
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout<< "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    (void)window; // Prevent the -Werror=unused-parameter error during compilation.
+    glViewport(0, 0, width, height);
 }
