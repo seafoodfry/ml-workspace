dcaab7b1eb245e6c6dd8e161757d057bf073e8c3
seafoodfry <>
2024-06-16 21:44:49 -0400

    Texture scaffold
    Signed-off-by: seafoodfry <>

---
+++ opengl-sandbox/learning-opengl/chapter-01-textures/main.c
@@ -0,0 +1,146 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <random>
+
+#include "shader.h"
+
+
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
+const char* textureImagePath = "./../resources/textures/container.jpg";
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+
+
+int main() {
+    // Instantiate the GLFW window.
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); needed on macos.
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout<< "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // Register the callback function for resizing.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Initialize GLAD to manage function pointer for openGL.
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout<< "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Load, compile the shaders and link them into a shader program.
+    Shader shader = Shader("shader.vert", "shader.frag");
+
+    /*
+        Configure vertex attributes.
+    */
+   float vertices[] = {
+        // positions         // colors
+         0.35f, -0.35f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right.
+        -0.35f, -0.35f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left.
+         0.0f,  0.35f, 0.0f,  0.0f, 0.0f, 1.0f   // top.
+    };
+
+    /*
+        1. Bind vertex array objects.
+        2. Copy our vertices arrays in a buffer for openGL to use.
+        3. Then set the vertex attribute pointers.
+    */
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Position attribute.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // Color attribute.
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
+    // The call glVertexAttribPointer() registered VBO as the vertex attribute's bound
+    // vertex buffer object, so we can safely unbind now.
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Now we can also unding the VAO so that other VAO calls won't modify this one.
+    glBindVertexArray(0);
+
+
+    GLuint texture = glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, &texture);
+    // Set the wrapping/filtering options on the currently bound texture.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+     int width, height, nrChannels;
+    unsigned chat* data = stbi_load(textureImagePath, &width, &height, &nrChannels, 0);
+    if (!data) {
+        std::cerr << "Failed to load texture image: " << textureImagePath << std::endl;
+        return -1;
+    }
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipMap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Process input.
+        processInput(window);
+
+        // Render.
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw.
+        shader.use();
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    // glDeleteProgram(shader.ID); is now done by the Shader destructor.
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    (void)window; // Prevent the -Werror=unused-parameter error during compilation.
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
