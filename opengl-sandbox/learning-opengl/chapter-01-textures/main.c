4a57eb1bffe427dca8c99ea4a558a198432bb751
seafoodfry <>
2024-06-18 19:22:52 -0400

    Working texture example
    Signed-off-by: seafoodfry <>

--- opengl-sandbox/learning-opengl/chapter-01-textures/main.c
+++ opengl-sandbox/learning-opengl/chapter-01-textures/main.c
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <stb_image.h>
 
 #include <iostream>
 #include <fstream>
@@ -61,20 +61,29 @@ int main() {
         -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
         -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
     };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
 
     /*
         1. Bind vertex array objects.
         2. Copy our vertices arrays in a buffer for openGL to use.
         3. Then set the vertex attribute pointers.
     */
-    GLuint VAO, VBO;
+    GLuint EBO, VAO, VBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // Position attribute.
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -93,23 +102,26 @@ int main() {
     glBindVertexArray(0);
 
 
-    GLuint texture = glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, &texture);
-    // Set the wrapping/filtering options on the currently bound texture.
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    // Load and create the texture.
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-     int width, height, nrChannels;
-    unsigned chat* data = stbi_load(textureImagePath, &width, &height, &nrChannels, 0);
+    // Load image, create a texture, and generate mipmaps.
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(textureImagePath, &width, &height, &nrChannels, 0);
     if (!data) {
         std::cerr << "Failed to load texture image: " << textureImagePath << std::endl;
         return -1;
     }
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipMap(GL_TEXTURE_2D);
-
+    glGenerateMipmap(GL_TEXTURE_2D);
     stbi_image_free(data);
 
     while (!glfwWindowShouldClose(window)) {
@@ -123,8 +135,11 @@ int main() {
         // Draw.
         shader.use();
 
+        glBindTexture(GL_TEXTURE, texture);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
