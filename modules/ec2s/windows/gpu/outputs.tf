50c40f6b05b71284531861ebaad7955e623957ad
seafoodfry <99568361+seafoodfry@users.noreply.github.com>
2024-06-22 11:38:22 -0400

    Created module for windows gpus
    Signed-off-by: seafoodfry <99568361+seafoodfry@users.noreply.github.com>

---
+++ modules/ec2s/windows/gpu/outputs.tf
@@ -0,0 +1,9 @@
+output "public_dns" {
+  value       = aws_instance.ec2.public_dns
+  description = "EC2 Public DNS"
+}
+
+output "instance_id" {
+  value = aws_instance.ec2.id
+  description = "Instance ID"
+}
