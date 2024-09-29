91b8ace766ce49b5e53828d5d732be45a9be445a
seafoodfry <99568361+seafoodfry@users.noreply.github.com>
2024-09-29 11:10:55 -0500

    Created TF module for F1 instances
    Signed-off-by: seafoodfry <99568361+seafoodfry@users.noreply.github.com>

---
+++ gpu-sandbox/ec2_fpga.tf
@@ -0,0 +1,20 @@
+# ami-0762d104e2c4fb01f is the "FPGA Developer" AMI based on CentOS.
+# There is also one based on AL2, but funny enough that one is easier
+# to find when going through the marketplace.
+module "fpga" {
+  count  = 0
+  source = "../modules/ec2s/linux/fpga"
+
+  name              = "dev-fpga"
+  ami               = "ami-0762d104e2c4fb01f"
+  type              = "f1.2xlarge"
+  security_group_id = aws_security_group.ssh.id
+  subnet_id         = module.vpc.public_subnets[0]
+  ec2_key_name      = var.ec2_key_name
+
+  instance_profile_name = aws_iam_instance_profile.dcv.name
+}
+output "fpga" {
+  value       = module.fpga[*].public_dns
+  description = "Public dev DNS"
+}
---
+++ modules/ec2s/linux/fpga/setup.sh.tpl
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+
+USERNAME="ec2-user"
+
+os_name=$(grep ^ID= /etc/os-release | cut -d'=' -f2 | tr -d '"')
+echo "working on $os_name..."
+if [ "$os_name" == "ubuntu" ]; then
+    echo "Running on Ubuntu"
+    USERNAME="ubuntu"
+else
+    echo "Unknown OS"
+fi
+
+date > /home/$USERNAME/CLOUDINIT-STARTED
+
+
+sudo yum update -y
+sudo yum install -y vim
+
+
+
+
+
+date > /home/$USERNAME/CLOUDINIT-COMPLETED
