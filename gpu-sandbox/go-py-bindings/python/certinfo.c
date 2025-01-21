93e47c7185e59f388f2b9e2534ee1b7979667633
seafoodfry <99568361+seafoodfry@users.noreply.github.com>
2025-01-20 22:39:28 -0600

    Added libcurl certinfo example
    Signed-off-by: seafoodfry <99568361+seafoodfry@users.noreply.github.com>

---
+++ gpu-sandbox/go-py-bindings/python/certinfo.c
@@ -0,0 +1,55 @@
+#include <curl/curl.h>
+#include <stdio.h>
+
+// https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
+static size_t wrfu(void *ptr, size_t size, size_t nmemb, void *userdata) {
+  (void)ptr;       // Points to the delivered data.
+  (void)userdata;  // User-provided data pointer: used to share state between
+                   // callbacks and your main program.
+  return size * nmemb;
+}
+
+int main(void) {
+  CURL *curl;
+  CURLcode res;
+
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.seafoodfry.ninja/");
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, wrfu);
+
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
+
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    curl_easy_setopt(curl, CURLOPT_CERTINFO, 1L);
+
+    res = curl_easy_perform(curl);
+    if (!res) {
+      struct curl_certinfo *certinfo;
+
+      res = curl_easy_getinfo(curl, CURLINFO_CERTINFO, &certinfo);
+      if (!res && certinfo) {
+        int i;
+
+        printf("%d certs!\n", certinfo->num_of_certs);
+
+        for (i = 0; i < certinfo->num_of_certs; i++) {
+          struct curl_slist *slist;
+
+          for (slist = certinfo->certinfo[i]; slist; slist = slist->next)
+            printf("%s\n", slist->data);
+        }
+      }
+    }
+
+    curl_easy_cleanup(curl);
+  }
+
+  curl_global_cleanup();
+
+  return 0;
+}
